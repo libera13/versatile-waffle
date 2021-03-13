c8b5248142a4d06a6e97b0711043a669f5a0047a
Emil <emillibera@gmail.com>
2021-03-13 21:23:01 +0100

    feat: basic MicroOrm Configuration

---
+++ server/README.md
@@ -0,0 +1,5 @@
+### Prerequisites
+- Installed postgres. [How to on Ubuntu 18.04](https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-18-04)
+1. Create db 
+
+```sudo -u postgres createdb versatile```
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Haha')
+import { MikroORM } from "@mikro-orm/core"
+import {PROD} from "./constants";
+
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [],
+        dbName: 'versatile',
+        type : 'postgresql',
+        debug: !PROD,
+        user: 'postgres',
+        password: ''
+    })
+
+}
+
+
+main()
+console.log('Hahajja')
+// const a = 2;
